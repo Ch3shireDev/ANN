Still trying to fix the gradient training

main.py
@@ -6,8 +6,9 @@ from numpy.random import randn
 from neural_network import network
 from itertools import product
 
-net = network([1,3,3,1], filename='./data/x')
+net = network([1,10,10,1], filename='./data/x')
 net.load_random()
+# net.load()
 
 x = [[x] for x in np.linspace(0,1,4)]
 y = [[np.random.random()] for _ in range(4)]
@@ -15,53 +16,56 @@ y = [[np.random.random()] for _ in range(4)]
 X = [xx[0] for xx in x]
 Y = [yy[0] for yy in y]
 
-dw = 0.001
 
-# E = []
-# for w in net.W:
-#     E += [np.zeros_like(w)]
-# E = np.array(E)
+Wmin =-10
+Wmax = 10
+n=8
 
-# indices = []
-# for i in range(len(E)):
-#     for j in range(len(E[i])):
-#         for k in range(len(E[i][j])):
-#             indices += [(i,j,k)]
+Wzero = net.W.copy()
 
-# for ii in range(50):
-#     print([np.linalg.norm(e) for e in E])
-#     for kk in range(len(x)):
-#         grad = E.copy()
-#         x0, y0 = x[kk], y[kk]
-#         y1 = net.cost(x0,y0,net.W)
-#         for i,j,k in indices:
-#             E[i][j][k] = 1
-#             y2 = net.cost(x0,y0,net.W + E*dw)
-#             grad[i][j][k] += (y2-y1)/dw
-#             E[i][j][k] = 0
+for i in range(len(Wzero)):
+    Wzero[i] = np.zeros_like(Wzero[i])
 
-#         for i in range(len(grad)):
-#             g = np.linalg.norm(grad[i])
-#             if g>1e-6:
-#                 grad[i] /= g
-        
-#         wmin,wmax = -100, 100
-#         n = 8
-#         for _ in range(10):
-#             Ws = np.linspace(wmin,wmax,n+1)
-#             c = []
-#             for w in Ws:
-#                 c += [net.cost(x0,y0,net.W+w*grad)]
-#             index = np.argmin(c)
-#             c = c[index]
-#             w = Ws[index]
-#             dw = (wmax-wmin)/n
-#             wmin, wmax = w-dw, w+dw
-#         net.W += w*grad
-#     print(ii, c, [np.linalg.norm(w) for w in net.W], dw)
+indices = []
 
-for i in range(50):
-    net.retarded_training(x,y)
+for k in range(len(Wzero)):
+    a, b = Wzero[k].shape
+    for l, m in product(range(a), range(b)):
+        indices += [(k,l,m)]
+
+for ii in range(20):
+    dW = Wzero.copy()
+    Wout = Wzero.copy()
+    for k, l, m in indices:
+        W = net.W
+        wmin = Wmin
+        wmax = Wmax
+        for i in range(10):
+            Ws = np.linspace(wmin, wmax, n+1)
+            C = []
+            for w in Ws:
+                W[k][l][m] = w
+                costs = net.cost(x, y, W)
+
+                # dW[k][l][m] = w
+                # costs = net.cost(x,y,net.W+dW)
+                # dW[k][l][m] = 0
+
+                C += [sum(costs)]
+
+            index = np.argmin(C)
+            w = Ws[index]
+            c = C[index]
+            dw = (wmax - wmin)/n
+            wmin, wmax = w - dw, w + dw
+
+        # dW[k][l][m] = w
+        # Wout[k][l][m] = w
+        # Wout[k][l][m] += dW[k][l][m]
+
+    net.W = np.array([w.copy() for w in W])
+
+    print(ii,c,[np.linalg.norm(e) for e in net.W])
 
 net.save()
 
@@ -77,4 +81,5 @@ for x in X:
 
 plt.plot(X,np.array(Y))
 plt.show()
+
 