34b27a78366c8bd88fb2978e517d20b3bfcdf643
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2018-12-22 20:43:27 +0100

    New upload

--- neural2.py
+++ neural2.py
@@ -7,32 +7,33 @@ def nonlin(x,deriv=False):
 def add_one(X):
     return np.array([np.array(X.T[0]), np.ones(X.shape[0])]).T
 
-X = np.array([np.linspace(0, 1, 128)]).T   
+X = np.array([np.linspace(0, 1, 128)]).T
 y = np.sin(X*np.pi*4)/2+0.5
 
 l0 = X
 
 num_inputs = 1
 num_outputs = 1
-HL = [num_inputs, 8, num_outputs]
+HL = [num_inputs, 8, 8, num_outputs]
 
 
 syn = []
 syn += [2*np.random.random((HL[0] + 1, HL[1])) - 1]
 syn += [2*np.random.random((HL[1] + 1, HL[2])) - 1]
+syn += [2*np.random.random((HL[2], HL[3])) - 1]
 error = 0.5
 
 
-for i in range(1000):
+while True:
     L = [add_one(l0)]
-    a = syn[0]
-    a = nonlin(np.dot(L[0], a))
 
-    b = np.array([np.ones(X.shape[0])]).T
-    a = np.concatenate((a,b),axis=1)
+    L += [nonlin(np.dot(L[0], syn[0]))]
 
-    L += [a]
+    L[-1] = np.concatenate([L[-1].T, np.array([np.ones(128)])]).T
+
     L += [nonlin(np.dot(L[1], syn[1]))]
+    L += [nonlin(np.dot(L[2], syn[2]))]
+
     Y = L[-1]
 
 
@@ -40,20 +41,33 @@ for i in range(1000):
 
     error = y - L[-1]
     
+    delta = error*nonlin(L[3], deriv=True)
+    d_syn = [L[2].T.dot(delta)] + d_syn
+    error = delta.dot(syn[2].T)
+
     delta = error*nonlin(L[2], deriv=True)
     d_syn = [L[1].T.dot(delta)] + d_syn
-    error = delta.dot(syn[1].T)
+    error = delta.dot(syn[1].T).T[:-1].T
 
-    delta = error*nonlin(L[1], deriv=True)
+    delta = error*nonlin(L[1].T[:-1].T, deriv=True)
     d_syn = [L[0].T.dot(delta)] + d_syn
+    # error = delta.T[:-1].T.dot(syn[0].T)
     error = delta.dot(syn[0].T)
 
-    syn[0] += d_syn[0]
-    syn[1] += d_syn[1]
 
-error = np.mean(np.abs(y - L[-1]))
+    syn[0] += d_syn[0]
+    # syn[0] += d_syn[0].T[:-1].T
+    syn[1] += d_syn[1]
+    syn[2] += d_syn[2]
+
+    error = np.mean(np.abs(y - L[-1]))
+    
+    print(error)
+    if error<0.01:
+        break
+
 
 print(error)
-plt.plot(X, y)
-plt.plot(X, Y)
+plt.plot(y)
+plt.plot(Y)
 plt.show()
